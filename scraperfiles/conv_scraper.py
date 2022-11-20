05aa1b540ef99fb95a0029e674e58ad51bf015a1
cmason30 <colinpetermason@gmail.com>
2022-11-19 19:01:57 -0700

    added mongodb compatibility

--- scraperfiles/conv_scraper.py
+++ scraperfiles/conv_scraper.py
@@ -8,7 +8,7 @@ cwd_ = os.getcwd()
 #connString = os.environ['MONGODB_CONNSTRING']
 client = pymongo.MongoClient('localhost', 27017)
 
-mydb = client['citytest1']
+mydb = client['citytest1' # Set up
 posts = mydb.posts
 
 with open(f'{cwd_}/scraperfiles/reddit_access.json', 'r') as f:
@@ -38,6 +38,7 @@ for city in cities_list:
         if submission.selftext == "":
             self_text = "empty"
         else:
+
             self_text = submission.selftext
 
         city_dict = {
