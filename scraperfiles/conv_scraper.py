1ecdd4e83686d9551d7ac2474a825dcab0cd887e
cmason30 <colinpetermason@gmail.com>
2022-12-06 19:21:41 -0700

    test data

--- scraperfiles/conv_scraper.py
+++ scraperfiles/conv_scraper.py
@@ -40,58 +40,48 @@ cities_list = cities_json['City List']
 
 cities_str = '+'.join(cities_list)
 
-bulk_subs = []
+bulk_subs = {}
 
 count_ = 0
 for city in cities_list:
+    city_list = []
+    subreddit = reddit.subreddit(city)
     for submission in reddit.subreddit(city).top(time_filter="day", limit=10):
 
         if submission.selftext == "":
             self_text = "empty"
         else:
-
             self_text = submission.selftext
 
-        #comments = [i.body for i in submission.comments]
         submission.comments.replace_more(limit=0)  # flatten tree
         comments = submission.comments.list()  # all comments
 
         f_com = []
         for comment in comments:
-            com_list = []
             comment_rem = re.sub(r'http\S+|[^\w\s]', '', comment.body)
-            #omment_rem = re.sub(r'[^\w\s]', '', comment_rem)
+            f_com.append(comment_rem)
 
-            print(model(comment_rem), comment_rem)
-            #word_tokens = word_tokenize(comment_rem)
-            # for w in word_tokens:
-            #     if w not in stop_words:
-            #         com_list.append(w)
-            # f_com.append(com_list)
-
-        mean_l = []
-        for i in f_com:
-            com = " ".join(i)
-            mean_l.append(sia.polarity_scores(com)['compound'])
-
-        city_dict = {
-            "subreddit": city,
+        submission_dict = {
+            #"subreddit": city,
             "created_utc": submission.created_utc,
             "num_comments": submission.num_comments,
             "selftext": self_text,
             "title": submission.title,
             "comments": f_com,
-            "lat": cities_list[city]['lat'],
-            "lon": cities_list[city]['lon']
+            #"lat": cities_list[city]['lat'],
+            #"lon": cities_list[city]['lon']
         }
 
-        bulk_subs.append(city_dict)
+        city_list.append(submission_dict)
         break
-    if count_ == 15:
+
+    bulk_subs[city] = city_list
+    if count_ == 5:
         break
     count_ += 1
     break
 
+print(bulk_subs)
 with open('test1_data.pickle', 'wb') as handle:
     pickle.dump(bulk_subs, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
