7e0c471665c2166a9b1cca8cebff976ef8d50472
cmason30 <colinpetermason@gmail.com>
2022-12-06 21:33:45 -0700

    test data

--- scraperfiles/conv_scraper.py
+++ scraperfiles/conv_scraper.py
@@ -20,70 +20,76 @@ cwd_ = os.getcwd()
 # mydb = client['citytest1'] # Set up
 # posts = mydb.posts
 
-model = pipeline('sentiment-analysis', model="finiteautomata/bertweet-base-sentiment-analysis")
 
-with open(f'scraperfiles/reddit_access.json', 'r') as f:
-    creds = json.load(f)
+def load_files():
+    with open(f'scraperfiles/reddit_access.json', 'r') as f:
+        creds = json.load(f)
 
-reddit = praw.Reddit(
-    client_id=creds['Creds']['client_id'],
-    client_secret=creds['Creds']['client_secret'],
-    password=creds['Creds']['password'],
-    user_agent="testscript_",
-    username=creds['Creds']['account'],
-)
+    with open(f'scraperfiles/city_subreddits.json', 'r') as f:
+        cities_json = json.load(f)
 
-with open(f'scraperfiles/city_subreddits.json', 'r') as f:
-    cities_json = json.load(f)
+    return creds, cities_json
 
-cities_list = cities_json['City List']
 
-cities_str = '+'.join(cities_list)
+def extract_comments():
 
-bulk_subs = {}
+    creds, cities_json = load_files()
 
-count_ = 0
-for city in cities_list:
-    city_list = []
-    subreddit = reddit.subreddit(city)
-    for submission in reddit.subreddit(city).top(time_filter="day", limit=10):
+    reddit = praw.Reddit(
+        client_id=creds['Creds']['client_id'],
+        client_secret=creds['Creds']['client_secret'],
+        password=creds['Creds']['password'],
+        user_agent="testscript_",
+        username=creds['Creds']['account'],
+    )
 
-        if submission.selftext == "":
-            self_text = "empty"
-        else:
-            self_text = submission.selftext
+    cities_list = cities_json['City List']
+    cities_str = '+'.join(cities_list)
 
-        submission.comments.replace_more(limit=0)  # flatten tree
-        comments = submission.comments.list()  # all comments
+    count_ = 0
 
-        f_com = []
-        for comment in comments:
-            comment_rem = re.sub(r'http\S+|[^\w\s]', '', comment.body)
-            f_com.append(comment_rem)
+    for city in cities_list:
 
-        submission_dict = {
-            #"subreddit": city,
-            "created_utc": submission.created_utc,
-            "num_comments": submission.num_comments,
-            "selftext": self_text,
-            "title": submission.title,
-            "comments": f_com,
-            #"lat": cities_list[city]['lat'],
-            #"lon": cities_list[city]['lon']
-        }
+        all_subs_city = []
+        subreddit_ = reddit.subreddit(city)
 
-        city_list.append(submission_dict)
+        for submission in subreddit_.top(time_filter="day", limit=10):
+
+            if submission.selftext == "":
+                self_text = "empty"
+            else:
+                self_text = submission.selftext
+
+            submission.comments.replace_more(limit=0)  # flatten tree
+            comments = submission.comments.list()  # all comments
+
+            cleaned_com = [re.sub(r'http\S+|[^\w\s]', '', com.body) for com in comments]
+
+            submission_dict = {
+                #"subreddit": city,
+                "created_utc": submission.created_utc,
+                "num_comments": submission.num_comments,
+                "selftext": self_text,
+                "title": submission.title,
+                "comments": cleaned_com,
+                #"lat": cities_list[city]['lat'],
+                #"lon": cities_list[city]['lon']
+            }
+
+            all_subs_city.append(submission_dict)
+
+        cities_json['City List'][city]['data'] = all_subs_city
+
+        if count_ == 5:
+            break
+        count_ += 1
         break
 
-    bulk_subs[city] = city_list
-    if count_ == 5:
-        break
-    count_ += 1
-    break
+    print(cities_json)
+# with open('test1_data.pickle', 'wb') as handle:
+#     pickle.dump(bulk_subs, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-print(bulk_subs)
-with open('test1_data.pickle', 'wb') as handle:
-    pickle.dump(bulk_subs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+extract_comments()
 
 
 #unix_t = round(time.time())
