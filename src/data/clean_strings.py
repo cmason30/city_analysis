da155d4f77f776dbeca90d87030c8740d910e779
cmason30 <colinpetermason@gmail.com>
2022-12-07 12:40:42 -0700

    updated JSON output

--- src/data/clean_strings.py
+++ src/data/clean_strings.py
@@ -1,9 +1,19 @@
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
+import pickle
 
 def get_sentiment(comment):
     stopwords = nltk.corpus.stopwords.words("english")
     cleaned_text = ' '.join([word for word in comment.split() if word not in stopwords])
     sia = SentimentIntensityAnalyzer()
+
+    return sia.polarity_scores(cleaned_text)
+
+
+with open('/Users/colinmason/Desktop/python-projects/city_analysis/test1_data.pickle', 'rb') as f:
+    data = pickle.load(f)
+
+print(data)
+
 
 
